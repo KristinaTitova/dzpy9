3e20aa97a08adadd9ba6a213abd98f9d861fa80b
KristinaTitova <kristytitova@mail.ru>
2022-12-21 20:07:09 +0300

    бот

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from aiogram import Bot, Dispatcher, executor, types
+import telebot;
+bot = telebot.TeleBot('5650424674:AAGPs-FVez4B4ehEBoespe2-AzJEQ-CQndY');
+@dz_py9_bot(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+ if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+ elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши привет")
+ else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    bot.polling(none_stop=True, interval=0)
 
-API_TOKEN = '5650424674:AAGPs-FVez4B4ehEBoespe2-AzJEQ-CQndY'
-
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-async def send_welcome(message: types.Message):
-   await message.reply("Привет!\nЯ бот DZ!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
-async def echo(message: types.Message):
-   await message.answer(message.text)
-
-   if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-    
